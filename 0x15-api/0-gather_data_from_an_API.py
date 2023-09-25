689480a102da930922141718111756634abfaf74
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-09-25 14:26:11 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,61 +5,61 @@ from requests.exceptions import HTTPError
 from sys import argv
 
 
-
 def get_user(id):
-  """Get user by id"""
-  response = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-  if response.status_code != 200:
-    raise HTTPError(f'HTTP Error: {response.status_code}')
-  return response.json()
+    """Get user by id"""
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
+    if response.status_code != 200:
+        raise HTTPError(f'HTTP Error: {response.status_code}')
+    return response.json()
+
 
 def get_todos_by_user_id(id):
-  """Get User todos"""
-  response = requests.get(f"https://jsonplaceholder.typicode.com/todos")
-  if response.status_code != 200:
-    raise HTTPError(f'HTTP Error: {response.status_code}')
-  todos = response.json()
-  result = []
-  completed = 0;
-  for todo in todos:
-    if todo['userId'] == id:
-      result.append(todo)
-      if todo['completed'] == True:
-        completed += 1
-  return {
-    'todos': result,
-    'completed': completed
-  }
+    """Get User todos"""
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos")
+    if response.status_code != 200:
+        raise HTTPError(f'HTTP Error: {response.status_code}')
+    todos = response.json()
+    result = []
+    completed = 0
+    for todo in todos:
+        if todo['userId'] == id:
+            result.append(todo)
+            if todo['completed']:
+                completed += 1
+    return {
+        'todos': result,
+        'completed': completed
+    }
+
 
 def print_user_todos(user, todos):
-  """print user todos"""
-  result = f"Employee {user['name']} is done with tasks\
-({str(todos['completed'])}/{len(todos['todos'])}):\n"
-  for todo in todos['todos']:
-    if todo['completed']:
-      result += f"\t{todo['title']}\n"
-  
-  print(result)
+    """print user todos"""
+    result = f"Employee {user['name']} is done with tasks\
+  ({str(todos['completed'])}/{len(todos['todos'])}):\n"
+    for todo in todos['todos']:
+        if todo['completed']:
+            result += f"     {todo['title']}\n"
+    print(result)
 
 
 def main():
-  """entry point"""
-  if len(argv) != 2:
-    print("Usage: python3 0-gather_data_from_an_API.py ID ")
-    exit()
+    """entry point"""
+    if len(argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py ID ")
+        exit()
 
-  id = argv[1]
+    id = argv[1]
 
-  try:
-    id = int(id)
-    user = get_user(id)
-    todos = get_todos_by_user_id(id)
-    print_user_todos(user, todos)
-  except ValueError:
-    print("id must be integer")
-  except HTTPError:
-    print("something went wrong")
-  
+    try:
+        id = int(id)
+        user = get_user(id)
+        todos = get_todos_by_user_id(id)
+        print_user_todos(user, todos)
+    except ValueError:
+        print("id must be integer")
+    except HTTPError:
+        print("something went wrong")
+
 
 if __name__ == "__main__":
-  main()
+    main()
